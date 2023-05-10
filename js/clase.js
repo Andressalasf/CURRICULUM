6708233cb52a87f5f98fe7c7b6cd73f02ff5cbbe
Andressalasf <humkyjimp@gmail.com>
2023-05-10 17:06:42 -0500

    learning js

---
+++ js/clase.js
@@ -0,0 +1,36 @@
+//VARIABLES
+const nameuser = "Andressalas";
+var x = "Valor;"
+let y = "Valor";
+let a = 20;
+let b = 5.4;
+let string = "mi nombre es asndres";
+let boolean_uno= false;
+let array = [1,2,3,4,5];
+let array_string = ["lunes", "martes", "miercoles", "jueves", "viernes"]
+//operadores artimeticos
+let suma= 1+5+a;
+let resta = 1-5-a;
+let multi = 1*5*a;
+let divi = 1/5/a;
+let module = 10%2;
+let elevado= 5^2;
+console.log("-" +suma + "\n- "+ resta + "\n-" + multi + "\n- " + divi + "\n-"+ module);
+
+//OPERADORES DE COMPARACION = / == / ==== / <> / >= / <= / > / <
+
+//OPERADORES LOGICOS  / AND && / OR || /
+
+if (10>5){
+    for(let i=0; i<array_string.length; i++){
+        console.log(array_string[i]);
+    }
+
+}
+
+let j=5 ;
+let k = 10;
+while(k>=j){
+    console.log("Entra");
+    j++;
+}
